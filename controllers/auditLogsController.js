d9bf3590b539b547f6e2eb1cf1ab40e37dc4d20e
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-10-31 15:02:19 +0530

    Added the Date filters as well in the Audit Logs and enhanced the controller function to handle the filters efficiently

--- controllers/auditLogsController.js
+++ controllers/auditLogsController.js
@@ -1,28 +1,36 @@
 const { pool } = require("../database/db");
 
 const getAuditLogs = async (req, res) => {
-  const { action } = req.query;
+  const { action, start_date, end_date } = req.query;
   try {
-    let result;
-    if (!action) {
-      result = await pool.query(
-        "SELECT * FROM audit_logs ORDER BY created_at DESC"
-      );
-    } else {
-      result = await pool.query(
-        "SELECT * FROM audit_logs WHERE action ILIKE $1 ORDER BY created_at DESC",
-        [`${action}%`]
-      );
+    let query = "SELECT * FROM audit_logs WHERE 1=1";
+    const params = [];
+    let paramCount = 0;
+
+    if (action && action !== "" && typeof action === "string") {
+      paramCount++;
+      query += ` AND action ILIKE $${paramCount}`;
+      params.push(`${action}%`);
     }
-    const actions = await pool.query(`
-  SELECT DISTINCT 
-    SUBSTRING(action FROM '^[^:]+') as action_type
-  FROM audit_logs 
-  WHERE action LIKE '%:%'
-  ORDER BY action_type
-`);
-    console.log(actions);
-    
+
+    if (start_date) {
+      paramCount++;
+      query += ` AND DATE(created_at) >= $${paramCount}`;
+      params.push(start_date);
+    }
+
+    if (end_date) {
+      paramCount++;
+      query += ` AND DATE(created_at) <= $${paramCount}`;
+      params.push(end_date);
+    }
+
+    query += " ORDER BY created_at DESC";
+    const result = await pool.query(query, params);
+
+    const actions = await pool.query(
+      `SELECT DISTINCT SUBSTRING(action FROM '^[^:]+') as action_type FROM audit_logs  WHERE action LIKE '%:%' ORDER BY action_type`
+    );
     res.json({
       success: true,
       data: result.rows,
