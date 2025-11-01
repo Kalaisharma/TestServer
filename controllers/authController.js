d5013433610794e4b7de348d4d4895941b015cf0
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-11-01 16:15:23 +0530

    Add a new UserManagement and User Registration component in FE and the specified controllers and routes in backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,4 +76,13 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { login, register };
+const getUsers = async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM user_accounts");
+    return res.status(200).json({ users: users.rows });
+  } catch (error) {
+    console.error("Get users error:", error);
+    return res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+};  
+module.exports = { login, register, getUsers };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { login, register } = require("../controllers/authController");
+const { login, register, getUsers } = require("../controllers/authController");
 const verifyToken = require("../middleware/authmiddleware");
 // GET /protocols - Get all protocols
 router.post("/login", login);
 router.post("/register", verifyToken, register);
-
+router.get("/users", verifyToken, getUsers);
 module.exports = router;
