311a60f8339f55dca02ddad95aa6c2e40ed9b7fa
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-11-03 16:09:43 +0530

    refactor: update middleware import to remove default export for consistency in protocols route

--- routes/protocols.js
+++ routes/protocols.js
@@ -6,7 +6,7 @@ const {
   updateProtocol,
   getProtocolById,
 } = require("../controllers/protocolController");
-const verifyToken = require("../middleware/authmiddleware").default;
+const verifyToken = require("../middleware/authmiddleware");
 // GET /protocols - Get all protocols
 router.get("/protocols", verifyToken, getAllProtocols);
 
