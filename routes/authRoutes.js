e2dcc99126cf05078763c1fd327780355830131d
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-11-03 15:41:55 +0530

    feat: add updateUserStatus route and functionality to manage user status in authController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { login, register, getUsers, logout } = require("../controllers/authController");
+const { login, register, getUsers, logout, updateUserStatus } = require("../controllers/authController");
 const verifyToken = require("../middleware/authmiddleware");
 // GET /protocols - Get all protocols
 router.post("/login", login);
 router.post("/register", verifyToken, register);
 router.get("/users", verifyToken, getUsers);
 router.post("/logout", logout);
+router.put("/users/status/:id", verifyToken, updateUserStatus);
 module.exports = router;
